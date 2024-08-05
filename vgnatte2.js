61ca51c113e22302d5299de64c8cdcc98fd3c971
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2024-08-05 08:33:08 +0800

    Create vgnatte2.js

---
+++ vgnatte2.js
@@ -0,0 +1,1 @@
+(function(d,z,s,c){s.src='//'+d+'/400/'+z;s.onerror=s.onload=E;function E(){c&&c();c=null}try{(document.body||document.documentElement).appendChild(s)}catch(e){E()}})('goomaphy.com',7835583,document.createElement('script'),_alsciw)
